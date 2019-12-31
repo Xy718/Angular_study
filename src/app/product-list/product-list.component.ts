b2025b74f782831be269fdbbaf9af5f199264850
Xy718 <869839000@qq.com>
2019-12-31 20:38:54 +0800

    1

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent {
+  products = products;
+
+  share(proName) {
+    window.alert('The product '+proName+'has been shared!');
+  }
+}
