5177384cc6b088f479db850ffc435cac6bfcf760
Xy718 <869839000@qq.com>
2020-01-02 09:30:30 +0800

    33

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import{ products } from '../products';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product;
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService,
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params =>{
+      this.product=products[+params.get('productId')];
+    });
   }
 
+  addToCart(product){
+    window.alert('已添加到购物车！');
+    this.cartService.addToCart(product);
+  }
 }
